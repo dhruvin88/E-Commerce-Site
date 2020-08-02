cedf4829b2fb4fa7d800feb0818d2782d2352bd4
Dhruvin <dhruvinp895@gmail.com>
2020-08-02 14:12:53 -0500

    Added Nav to Project

--- e-commerce/src/components/app/App.js
+++ e-commerce/src/components/app/App.js
@@ -1,21 +1,40 @@
 import React from 'react';
-import logo from '../../images/logo.svg';
 import './App.css';
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from '@stripe/react-stripe-js';
 import { properties } from '../../properties.js';
 import CardForm from '../CardForm/CardForm';
 import { Container } from 'react-bootstrap';
+import { Switch, Route } from 'react-router-dom';
+import { Home, Page1, Page2, Page3 } from '../pages';
+import { Nav } from '../nav/nav';
 
 const stripePromise = loadStripe(properties.stripeKey);
 
+const Main = () => (
+  <main>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/1' component={Page1} />
+      <Route exact path='/2' component={Page2} />
+      <Route exact path='/3' component={Page3} />
+      <Route exact path='/pay'> 
+        <Elements stripe={stripePromise}>
+          <CardForm />
+        </Elements>
+      </Route>
+    </Switch>
+  </main>
+)
+
 function App() {
   return (
-    <Container>
-      <Elements stripe={stripePromise}>
-        <CardForm />
-      </Elements>
-    </Container>
+    <div>
+      <Nav></Nav>
+      <Container>
+        <Main></Main>
+      </Container>
+    </div>
   );
 }
 
---
+++ e-commerce/src/components/nav/nav.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export const Nav = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light  bg-light">
+        <Link className="navbar-brand" to="/">
+          Home
+        </Link>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/1">
+              Page1
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/2">
+              Page2
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/3">
+              Page3
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/pay">
+              Pay
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+};
